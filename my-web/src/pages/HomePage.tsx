fb116ac82528d74d22a638955b90d8415c8879c2
2kmkmkm <02dludals@gmail.com>
2024-08-05 16:03:07 +0900

    test: vercel

--- my-web/src/pages/HomePage.tsx
+++ my-web/src/pages/HomePage.tsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../components/common/atoms/Container";
 import Header from "../components/common/molecules/Header";
 import HomeButton from "../components/common/molecules/HomeButton";
+import { instance } from "../apis/instance";
 
 const HomePage: React.FC = () => {
+  const [info, setInfo] = useState({ role: "", name: "" });
+
+  useEffect(() => {
+    const fetchInfo = async () => {
+      try {
+        const response = await instance.get(`api/home`);
+        setInfo(response.data.body);
+        console.log(response);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchInfo();
+  }, []);
+
   return (
     <Container>
-      <Header
-        name="Fasten Julio Akbar"
-        backgroundColor="#40892d"
-        color="white"
-      />
-      <HomeButton role="user" />
+      {info && (
+        <>
+          <Header name={info.name} backgroundColor="#40892d" color="white" />
+          <HomeButton role={info.role} />
+        </>
+      )}
     </Container>
   );
 };
