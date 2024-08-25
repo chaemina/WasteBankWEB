23b3bc46812d59d712daaa3dc979988aa9ec5b8a
chaemina <chaemina82@naver.com>
2024-08-25 19:08:35 +0900

    fix :.env
    환경변수 숨기기

--- my-web/src/apis/instance.ts
+++ my-web/src/apis/instance.ts
@@ -6,7 +6,7 @@ axios.defaults.withCredentials = true;
 
 export const instance = axios.create({
   timeout: 5000,
-  baseURL: "http://ec2-43-202-58-157.ap-northeast-2.compute.amazonaws.com:8090",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 instance.interceptors.request.use(
