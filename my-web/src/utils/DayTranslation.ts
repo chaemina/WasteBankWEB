18325eb427a1a311899e8c9427eba73a66308e64
chaemina <chaemina82@naver.com>
2024-09-11 21:22:34 +0900

    fix

---
+++ my-web/src/utils/DayTranslation.ts
@@ -0,0 +1,10 @@
+export const dayTranslation: { [key: string]: string } = {
+    "월요일": "Senin",
+    "화요일": "Selasa",
+    "수요일": "Rabu",
+    "목요일": "Kamis",
+    "금요일": "Jumat",
+    "토요일": "Sabtu",
+    "일요일": "Minggu",
+  };
+  
